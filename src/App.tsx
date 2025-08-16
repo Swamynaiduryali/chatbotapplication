242cd5e516d27c03e1f6ad612e3f9e7db20fbfc9
Swamy Naidu Ryali <swamyr@tilli.pro>
2025-08-16 09:41:23 +0530

    new brach creation and creationg a component: marms branch

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Grid } from "./Components/Grid";
 import { Lines } from "./Components/Lines";
 import { Settings } from "./Components/Settings";
 import { Steps } from "./Components/Steps";
+import { MarmsGitLearning } from "./Components/MarmsGitLearning";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
           <Route path="lines" element={<Lines />} />
           <Route path="settings" element={<Settings />} />
           <Route path="blank" element={<Steps />} />
+          <Route path="marms" element={<MarmsGitLearning />} />
         </Route>
 
         {/* Example of a route without the sidebar */}
---
+++ src/Components/MarmsGitLearning.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const MarmsGitLearning = () => {
+  return (
+    <div>
+        <p>After creation of swamy brach and pushed the code to git later we started to create marms branch</p>
+        <p>Marms Git Learning: Create a new branch naming "marms" and then a component naming MarmsGitLearning</p>
+        <p>Later add commit push</p>
+    </div>
+  )
+}
