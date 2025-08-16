117acef8c3cd12b3f57d2ec3852e8addbbd8e9f9
Marms <marms12345@gmail.com>
2025-08-16 13:43:00 +0530

    merge main

--- src/Components/SwamyGitLearning.tsx
+++ src/Components/SwamyGitLearning.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
-export const SwamyGitLearning = () => {
-  return (
-    <div>
-        <p>Swamy git learning: Created a new branch namimg swamy and then a new component naming SwamyGitLearning</p>
-        <p>Later adding them to git and commit push to this branch</p>
-    </div>
-  )
+export const SwamyGitLearning: React.FC = () => {
+    return (
+        <div>
+            <p>I created a new branch named swamy and added a new component called SwamyGitLearning</p>
+            <p>After that, I staged the changes, committed them, and pushed to the swamy branch</p>
+            <p>Now I’m making changes again. Previously, I merged directly into main,
+                but this time I want to follow a different approach
+            </p>
+            <p>First, I will merge the latest changes from main into my swamy branch.
+                After resolving any conflicts, I will then merge the swamy branch back into main</p>
+        </div>
+    )
 }
