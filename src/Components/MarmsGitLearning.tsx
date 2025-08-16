74d41c492837bd6e530a47f95aa52bfb7f96f1a6
Marms <marms12345@gmail.com>
2025-08-16 13:56:08 +0530

    merge main

--- src/Components/MarmsGitLearning.tsx
+++ src/Components/MarmsGitLearning.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
-export const MarmsGitLearning = () => {
+export const MarmsGitLearning: React.FC = () => {
   return (
     <div>
-        <p>After creation of swamy brach and pushed the code to git later we started to create marms branch</p>
-        <p>Marms Git Learning: Create a new branch naming "marms" and then a component naming MarmsGitLearning</p>
-        <p>Later add commit push</p>
+        <p>After creating the swamy branch and pushing the code to Git, we then started working on a new branch called marms</p>
+        <p>Marms Git Learning:For Marms Git Learning, we created a new branch named marms and added a new component called MarmsGitLearning</p>
+        <p>After that, we staged the changes, committed them, and pushed the code to the marms branch</p>
+         <p>First, I will merge the latest changes from main into my marms branch.
+                After resolving any conflicts, I will then merge the marms branch back into main</p>
     </div>
   )
 }
